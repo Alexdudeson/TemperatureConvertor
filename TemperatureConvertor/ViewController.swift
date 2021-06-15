c17df9015568259bffee09e034fdc5f599b15311
Alexey Yarov <alexxd91@gmail.com>
2021-06-15 19:00:15 +0300

    Temperature convertor app, that converts Celsius into Fahrenheit + counts attempts

--- TemperatureConvertor/ViewController.swift
+++ TemperatureConvertor/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0   // we can do it also     var count: Int = 0
+    @IBOutlet weak var conversionNumLabel: UILabel!
+    @IBOutlet weak var tempEntry: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
-
+    @IBAction func convertClicked(_ sender: Any)
+    {
+        //print("Convert Clicked") <- for checking
+        if let result = tempEntry.text //checks text property
+        {
+            if (result == "")
+            {
+                return
+            }
+            else
+            {
+                if let num = Double(result)
+                {
+                    let output = num * (9/5) + 32
+                    resultLabel.text = String(output)
+                    
+                    updateCountByOne()
+                }
+            }
+        }
+    }
+    
+    func updateCountByOne()
+    {
+        count += 1 // or count = count + 1
+        conversionNumLabel.text = String(count) + " conversions"
+    }
+    
 }
 
