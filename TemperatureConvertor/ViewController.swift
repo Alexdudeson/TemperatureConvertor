cc0a0e974efd3326506cd843fbe6c831ab432467
Alexey Yarov <alexxd91@gmail.com>
2021-06-15 17:42:09 +0300

    Initial Commit

---
+++ TemperatureConvertor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemperatureConvertor
+//
+//  Created by Alexey Yarov on 15.06.2021.
+//  Copyright © 2021 Alexey Yarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
